8274ef90c3cd0fffd2ce3f38fc4e267f74aebf30
sherif altiby <sherifahmed852002@gmail.com>
2024-07-21 08:38:33 +0300

    add

---
+++ src/components/userDashboardComponents/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import MainHeader from "../../mainheader/MainHeader"
+
+const Dashboard = () => {
+  return (
+     <div className="user-dashboard-content">
+           <MainHeader title="لوحة التحكم" />
+     </div>
+  )
+}
+
+export default Dashboard
