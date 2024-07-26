81665501684cfb83ec309f38b348f3fbd9b7adfb
Ali Hadhood <ali.abdalla@msa.edu.eg>
2024-07-26 15:50:33 +0300

    api update

--- backend/platform/routes/api.php
+++ backend/platform/routes/api.php
@@ -13,7 +13,7 @@ Route::middleware('auth:sanctum')->group(function () {
         
     Route::get('/courses', [CourseController::class, 'index']);//get all courses
     Route::post('/courses', [CourseController::class, 'store']);//store the course
-    Route::get('/courses/{id}', [CourseController::class, 'show']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);//get the course id
     Route::put('/courses/{course}', [CourseController::class, 'update']);
     Route::delete('/courses/{course}', [CourseController::class, 'destroy']);
 
